ba2d2b7c4892f0b0e6f741b5798b6498c5643f9d
Kalaiyarasanmsdking <119731092+Kalaiyarasanmsdking@users.noreply.github.com>
2023-02-22 19:18:39 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,15 +5,15 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 2GB
+	Daily  Upload limit 1GB
 	Price 0
 	
 	**VIP** 
 	Daily  Upload  limit unlimited
-	Price Rs 150  🇮🇳/🌎 2.5$  per Month
+	Price Rs 40  🇮🇳/🌎 0.48$  per Month
 	
 	
-	Pay Using Upi I'd ```msmalikoffl@oksbi```
+	Pay Using Upi I'd ```kalaiyarasanmsd07@okhdfcbank```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
@@ -27,20 +27,20 @@ async def upgrade(bot,update):
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily  Upload limit 2GB
+	Daily  Upload limit 1GB
 	Price 0
 	
 	**VIP** 
 	Daily  Upload  limit unlimited 
-	Price Rs 150  🇮🇳/🌎 2.5$  per Month
+	Price Rs 40  🇮🇳/🌎 0.48$  per Month
 	
 	
-	Pay Using Upi I'd ```msmalikoffl@oksbi```
+	Pay Using Upi I'd ```kalaiyarasanmsd07@okhdfcbank```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN Contact🛂",url = "https://t.me/mrmalik_offl"), 
-        			InlineKeyboardButton("For Plan Upgrade 🌎",url = "https://t.me/mrmalik_offl")],
+        			InlineKeyboardButton("ADMIN Contact🛂",url = "https://t.me/MsdKalai07"), 
+        			InlineKeyboardButton("For Plan Upgrade 🌎",url = "https://t.me/MsdKalai07")],
 [InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await message.reply_text(text = text,reply_markup = keybord)
